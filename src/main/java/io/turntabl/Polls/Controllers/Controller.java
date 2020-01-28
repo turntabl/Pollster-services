ce102da6487c881193f6a26214f942cc6cb1f3ed
Dilyen <davidandohacquah@gmail.com>
2020-01-28 15:15:29 +0000

    first commit

--- src/main/java/io/turntabl/Polls/Controllers/Controller.java
+++ src/main/java/io/turntabl/Polls/Controllers/Controller.java
@@ -152,36 +152,5 @@ public class Controller implements DAO {
 
     //Creators Controller
 
-    @CrossOrigin
-    @ApiOperation("Get all Creators")
-    @Override
-    @GetMapping("/api/v1/creators")
-    public List<UserTO> getAllCreators() {
-        return this.template.query("select * from creators", new BeanPropertyRowMapper<UserTO>(UserTO.class));
-    }
 
-    @CrossOrigin
-    @ApiOperation("Add a Creator")
-    @Override
-    @PostMapping("/api/v1/creators")
-    public void AddNewCreator(UserTO creator) {
-        template.update("insert into creators (creator_id, creator_email, creator_name) values (?,?,?)", creator.getCreator_id(),creator.getCreator_email(), creator.getCreator_name());
-    }
-
-    @CrossOrigin
-    @ApiOperation("Get Creator by ID")
-    @Override
-    @GetMapping("/api/v1/creators/{id}")
-    public UserTO getCreatorById(@PathVariable("id") String id) {
-        return (UserTO)  template.queryForObject("select * from creators where creator_id = ?", new Object[]{id},
-                new BeanPropertyRowMapper(UserTO.class));
-    }
-
-    @CrossOrigin
-    @ApiOperation("Delete a Creator")
-    @Override
-    @DeleteMapping("/api/v1/creators/{id}")
-    public void deleteCreator(@PathVariable("id") String id) {
-        template.update("delete from creators where creator_id = ?", id);
-    }
 }
